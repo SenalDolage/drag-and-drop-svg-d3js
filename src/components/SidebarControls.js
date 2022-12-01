d1da812308b44ce006ab39e8ebe97da0d0a51c3b
Senal Dolage <senal.dolage@gmail.com>
2022-12-02 00:30:32 +0530

    sidebar controls sample buttons added

--- src/components/SidebarControls.js
+++ src/components/SidebarControls.js
@@ -2,13 +2,21 @@ import React from "react";
 
 export default function SidebarControls() {
   return (
-    <div>
+    <div className="sidebar-controls">
       <button type="button" className="btn btn-secondary">
         Zoom +
       </button>
       &nbsp;
       <button type="button" className="btn btn-secondary">
         Zoom -
+      </button>
+      &nbsp;
+      <button type="button" className="btn btn-secondary">
+        Save
+      </button>
+      &nbsp;
+      <button type="button" className="btn btn-secondary">
+        Clear all doors
       </button>
     </div>
   );
